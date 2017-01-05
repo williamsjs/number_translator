4e748eb54fa249cfae7cd37e71461892b810ba39
Joseph Williams <josephwilliams@Josephs-MacBook-Pro.local>
2017-01-04 19:28:33 -0500

    add 'q' exit function for main class

--- number_translator.rb
+++ number_translator.rb
@@ -1,12 +1,15 @@
 require './number_builder'
 
 def main
-  puts "Enter a number between -999,999 and 999,999 for translation: "
-  response = gets.chomp
+  response = ""
+  until response == "q"
+    puts "Enter a number between -999,999 and 999,999 for translation: "
+    response = gets.chomp
 
-  my_num = NumberBuilder.new(response)
+    my_num = NumberBuilder.new(response)
 
-  puts my_num.number_string
+    puts my_num.number_string if response != "q"
+  end
 end
 
 main
